d0c4c4484de06c063bb80568f9ad930a36f45e98
PrathyushNair <prathyu360@gmail.com>
2022-05-23 16:48:26 +0530

    change

--- src/components/TaskHeader/TaskHeader.jsx
+++ src/components/TaskHeader/TaskHeader.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import styles from "./taskHeader.module.css";
 
-const TaskHeader = () => {
+const TaskHeader = ({userdata,completed}) => {
   // sample values to be replaced
-  let totalTask = 0;
-  let unCompletedTask = 0;
-
+  let totalTask = userdata.length;
+  let unCompletedTask = completed.length;
+ console.log("in header",completed)
   // NOTE: do not delete `data-cy` key value pair
   return (
     <div data-cy="task-header" className={styles.taskHeader}>
-      <b data-cy="header-remaining-task">{unCompletedTask}</b>
-      <b data-cy="header-total-task">{totalTask}</b>
+      <h1>To Do List</h1>
+      <b data-cy="header-remaining-task"><p> pending Task </p>{unCompletedTask}</b>
+      <b data-cy="header-total-task"><p> Total Task </p>{totalTask}</b>
     </div>
   );
 };
